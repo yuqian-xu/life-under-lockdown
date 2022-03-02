e5f60810a02d77ca414af0e43752cbbe74380821
k21023767@kcl.ac.uk <sissie@xuyuqiandeMacBook-Pro.local>
2022-03-02 23:41:31 +0000

    creat a summary tibble with mean number of false beliefs for each level of know_facebook

--- life-under-lockdown-analysis.R
+++ life-under-lockdown-analysis.R
@@ -3,3 +3,14 @@ library(readr)
 
 lul_d <- read_csv("../data/life-under-lockdown.csv")
 
+lul_d %>%
+  mutate(know_facebook = ordered(
+    know_facebook,
+    levels = c(
+      "Nothing at all",
+      "Not very much",
+      "A fair amount",
+      "A great deal"
+    ))) %>%
+  group_by(know_facebook) %>%
+  summarise(n = n(), Beliefs = mean(beliefs))
